3b064e88ab74846bc6e39bad31bbc2cf46c6a6e4
Javeed798 <karishmavahida@gmail.com>
2024-08-02 01:10:26 +0530

    Initial Setup of Layout page done using resizable from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import Image from "next/image";
+import dynamic from "next/dynamic";
 
 export default function Home() {
-   return (
-      <div className={"flex w-screen flex-col h-screen bg-background"}>
-         <div className={"h-12 flex"}>
-            Navbar
-         </div>
-         <div className={"w-screen flex grow bg-red-600"}>
-            Code Editor
-         </div>
+  const CodeEditor = dynamic(() => import("@/components/editor"), {
+    ssr: false,
+  });
+
+  return (
+    <div className={"flex w-screen flex-col h-screen bg-background"}>
+      <div className={"h-12 flex"}>Navbar</div>
+      <div className={"w-screen flex grow"}>
+        <CodeEditor />
       </div>
-   );
+    </div>
+  );
 }
---
+++ components/editor/index.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import React, { useRef } from "react";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
+import { Editor, OnMount } from "@monaco-editor/react";
+import monaco from "monaco-editor";
+
+export default function CodeEditor() {
+  const editorRef = useRef<null | monaco.editor.IStandaloneCodeEditor>(null);
+
+  const handelEditorMount: OnMount = (editor, monaco) => {
+    editorRef.current = editor;
+  };
+
+  return (
+    <>
+      <div className={"h-full w-52"}></div>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel
+          maxSize={75}
+          minSize={30}
+          defaultSize={30}
+          className={"flex flex-col p-2"}
+        >
+          <div className={"h-10 w-full flex gap-2"}>
+            <Button
+              variant={"secondary"}
+              size={"sm"}
+              className={"min-w-20 justify-between"}
+            >
+              index.html <X className={"w-3 h-3"} />
+            </Button>
+            <Button
+              variant={"secondary"}
+              size={"sm"}
+              className={"min-w-20 justify-between"}
+            >
+              style.css <X className={"w-3 h-3"} />
+            </Button>
+          </div>
+          <div className="grow w-full overflow-hidden">
+            <Editor
+              height={"100%"}
+              defaultLanguage="typescript"
+              theme="vs-dark"
+              onMount={handelEditorMount}
+            />
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={40}>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel
+              defaultSize={50}
+              minSize={20}
+              className="p-2 flex flex-col"
+            >
+              <div className={"h-10 w-full flex gap-2"}>
+                <Button
+                  variant={"secondary"}
+                  size={"sm"}
+                  className={"min-w-20 justify-between"}
+                >
+                  localhost:3000 <X className={"w-3 h-3"} />
+                </Button>
+              </div>
+              <div className="w-full grow rounded-lg bg-foreground"></div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel
+              defaultSize={50}
+              minSize={20}
+              className="p-2 flex flex-col"
+            >
+              <div className={"h-10 w-full flex gap-2"}>
+                <Button
+                  variant={"secondary"}
+                  size={"sm"}
+                  className={"min-w-20 justify-between"}
+                >
+                  Node <X className={"w-3 h-3"} />
+                </Button>
+              </div>
+              <div className="w-full grow rounded-lg bg-foreground"></div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </>
+  );
+}
